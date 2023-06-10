bb3648c623aba4f65881e7fd412e6715f78ba5a0
tiagoms <tiagoms@ciandt.com>
2023-06-10 13:30:05 -0300

    Criando middlewares de JSON

--- src/server.js
+++ src/server.js
@@ -14,6 +14,7 @@
 */
 
 import http from 'node:http'
+import { json } from './middlewares/json.js' // Para não dar erro module not found, colocar a extensão do arquivo .js no import, isso porque no package.json o type está como module
 
 const users = []
 
@@ -21,20 +22,10 @@ const server =  http.createServer(async (req, res) => {
 
     const { method, url } = req
 
-    const buffers = []
-
-    for await (const chunk of req){
-        buffers.push(chunk)
-    }
-    
-    try {
-        req.body = JSON.parse(Buffer.concat(buffers).toString())
-    } catch {
-        req.body = null
-    }
+   await json(req, res)
     
     if (method === 'GET' && url === '/users'){
-        return res.setHeader('Content-type', 'application/json').end(JSON.stringify(users))
+        return res.end(JSON.stringify(users))
     }
 
     if (method === 'POST' && url === '/users'){
