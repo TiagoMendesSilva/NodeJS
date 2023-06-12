7a0c617862c075d4dd4048b58c7199bbe0b08f59
tiagoms <tiagoms@ciandt.com>
2023-06-11 21:47:23 -0300

    Separando rotas da aplicação

--- src/server.js
+++ src/server.js
@@ -15,38 +15,24 @@
 
 import http from 'node:http'
 import { json } from './middlewares/json.js' // Para não dar erro module not found, colocar a extensão do arquivo .js no import, isso porque no package.json o type está como module
-import { Database } from './database.js'
-import { randomUUID } from 'node:crypto'
-
-const database = new Database()
+import { routes } from './routes.js'
 
 const server =  http.createServer(async (req, res) => {
-
+    
     const { method, url } = req
 
-   await json(req, res)
+    await json(req, res)
     
-    if (method === 'GET' && url === '/users'){
+    const route = routes.find(route => {
+        return route.method === method && route.path === url
 
-        const users = database.select('users')
+    })
 
-        return res.end(JSON.stringify(users))
+    if(route){
+        return route.handler(req,res)
     }
 
-    if (method === 'POST' && url === '/users'){
-                
-        const { name, email } = req.body      
-        
-        const user = {
-            id: randomUUID(),
-            name,
-            email
-        }
 
-        database.insert('users', user)
-
-        return res.writeHead(201).end()
-    }
 
     return res.writeHead(404).end()
 })
