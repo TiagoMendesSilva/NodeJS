50eced9a12453a2dd80232e733f142419b4a8b54
tiagoms <tiagoms@ciandt.com>
2023-06-13 14:07:34 -0300

    Atualizando registros pelo id e atualizando req.body = { name e email}

--- src/database.js
+++ src/database.js
@@ -40,6 +40,16 @@ export class Database {
 
     }
 
+    update(table,id, data){
+        const rowIndex = this.#database[table].findIndex(row => row.id === id)
+      
+        if(rowIndex > -1){
+            this.#database[table][rowIndex] = {id, ...data}
+        }
+
+        this.#persit()
+    }
+
     delete(table,id){
         const rowIndex = this.#database[table].findIndex(row => row.id === id)
 
